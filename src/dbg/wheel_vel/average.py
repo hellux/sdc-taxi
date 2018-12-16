8cdcc2505da66f6cf3a33bdb6cd902e0244fa161
Noah Hellman <noah.hellman@protonmail.com>
2018-12-16 14:41:11 +0100

    test plotting average velocity of period

--- src/dbg/wheel_vel/average.py
+++ src/dbg/wheel_vel/average.py
@@ -9,12 +9,10 @@ w = []
 period = 10
 
 for line in sys.stdin:
-    _t, _v, _w = map(float, line.split())
-    if _v > 0:
-        non_zero = True
-        t.append(_t)
-        v.append(_v)
-        w.append(_w)
+    values = list(map(float, line.split()))
+    t.append(values[0])
+    v.append(values[1])
+    w.append(values[2])
 
 p_sum = 0
 avg = []
@@ -38,3 +36,41 @@ while i < len(t)-(len(t)%period):
         p_offs = []
     i += 1
 
+period = 5
+va = [0 for _ in range(period)]
+vi = 0
+vs = 0
+v5 = []
+v10 = []
+for _v in v:
+    if _v == 0:
+        vs = 0
+        v5.append(0)
+        vi = 0
+    else:
+        vs -= va[vi]
+        vs += _v
+        va[vi] = _v
+        vi = (vi + 1) % period
+        v5.append(vs/period)
+
+period = 10
+va = [0 for _ in range(period)]
+vi = 0
+vs = 0
+v10 = []
+for _v in v:
+    if _v == 0:
+        vs = 0
+        v10.append(0)
+        vi = 0
+    else:
+        vs -= va[vi]
+        vs += _v
+        va[vi] = _v
+        vi = (vi + 1) % period
+        v10.append(vs/period)
+
+with open('avg.dat', 'w') as f:
+    for _t, _v, _w, _v5, _v10 in zip(t, v, w, v5, v10):
+        f.write('{} {} {} {} {}\n'.format(_t, _v, _w, _v5, _v10))
